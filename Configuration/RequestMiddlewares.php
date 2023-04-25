d97ed8bc789c26da558954d58a98675318786216
Matthias Vogel <m.vogel@andersundsehr.com>
2023-04-25 12:04:25 +0200

    🐛 fix middleware possition

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -7,8 +7,15 @@ return [
         'a-u-s/reduce-duplicate-content/reduce-duplicate-content-middleware' => [
             'target' => ReduceDuplicateContentMiddleware::class,
             'after' => [
+                'typo3/cms-frontend/base-redirect-resolver',
+                'typo3/cms-frontend/static-route-resolver',
                 'typo3/cms-frontend/page-resolver',
             ],
+            'before' => [
+                'typo3/cms-frontend/preview-simulator',
+                'typo3/cms-frontend/page-argument-validator',
+                'typo3/cms-frontend/tsfe',
+            ]
         ],
     ],
 ];
